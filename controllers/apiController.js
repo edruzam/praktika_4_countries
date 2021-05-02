d692b04a381cd747ed1d705f6e8ccbd385e5f46a
edruzam <83424964+edruzam@users.noreply.github.com>
2021-05-02 21:52:42 +0300

    subtask D done

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -93,6 +93,57 @@ module.exports = function (app) {
             });
     });
 
-
+    //d. näidata täielikku teavet määratud riigi kohta (teave riigi ja linnade kohta). Andmete lugemiseks looge ka 2 päringut: riigi koodi ja nime järgi. 
+    app.get("/api/countrybyname/:name", function (req, res) {
+        db.any(
+        "SELECT c.code as country_code, c.name as country_name, c.continent, c.region, c.surfacearea, " +
+        "c.indepyear, c.population as country_population, c.lifeexpectancy, c.gnp, c.gnpold, c.localname, c.governmentform, " +
+        "c.headofstate, c.capital, c.code2, city.id as city_id, trim(from city.name) city_name, " +
+        "trim(from city.district) city_district, city.population as city_population " +
+        "FROM city " +
+        "INNER JOIN country as c ON c.code=city.countrycode " +
+        "WHERE c.name LIKE '%" + req.params.name + "%':: varchar"
+        )
+        .then(function (data) {
+            res.json({
+                status: "success",
+                data: data,
+            });
+        })
+        .catch(error => {
+            if (Array.isArray(error) && 'getErrors' in error) {
+                // the error came from method `batch`;
+                // let's log the very first error:
+                error = error.getErrors()[0];
+            }
+            console.log("ERROR:", error.message || error);
+        });
+    });
     
+    //d. näidata täielikku teavet määratud riigi kohta (teave riigi ja linnade kohta). Andmete lugemiseks looge ka 2 päringut: riigi koodi ja nime järgi. 
+    app.get("/api/countrybycode/:code", function (req, res) {
+        db.any(
+        "SELECT c.code as country_code, c.name as country_name, c.continent, c.region, c.surfacearea, " +
+        "c.indepyear, c.population as country_population, c.lifeexpectancy, c.gnp, c.gnpold, c.localname, c.governmentform, " +
+        "c.headofstate, c.capital, c.code2, city.id as city_id, trim(from city.name) city_name, " +
+        "trim(from city.district) city_district, city.population as city_population " +
+        "FROM city " +
+        "INNER JOIN country as c ON c.code=city.countrycode " +
+        "WHERE c.code='" + req.params.code + "':: varchar"
+        )
+        .then(function (data) {
+            res.json({
+                status: "success",
+                data: data,
+            });
+        })
+        .catch(error => {
+            if (Array.isArray(error) && 'getErrors' in error) {
+                // the error came from method `batch`;
+                // let's log the very first error:
+                error = error.getErrors()[0];
+            }
+            console.log("ERROR:", error.message || error);
+        });
+    });
 };
