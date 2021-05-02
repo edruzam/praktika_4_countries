11bd52e0972fe6ef348ada7dcfc3b1deff88f1ad
edruzam <83424964+edruzam@users.noreply.github.com>
2021-05-02 21:18:37 +0300

    Subtask C and cleanup

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -17,11 +17,13 @@ module.exports = function (app) {
                     data: data,
                 });
             })
-            .catch((err) => {
-                res.json({
-                    description: "Can't find any country",
-                    error: err,
-                });
+            .catch(error => {
+                if (Array.isArray(error) && 'getErrors' in error) {
+                    // the error came from method `batch`;
+                    // let's log the very first error:
+                    error = error.getErrors()[0];
+                }
+                console.log("ERROR:", error.message || error);
             });
     });
 
@@ -48,76 +50,49 @@ module.exports = function (app) {
         });
     });
 
-    // Näidata, millised kontrollerid on andmebaasis
-    app.get("/api/controllers", function (req, res) {
-        db.any("SELECT DISTINCT controller FROM controller")
+
+    // c. näidata täielikku teavet määratud linna kohta (2 GET - päringut: linnakoodi ja nime järgi) 
+    app.get("/api/citybyname/:name", function (req, res) {
+        db.any(
+            "SELECT id, trim(from name) city_name, countrycode, trim(from district) city_district, population FROM city WHERE name LIKE '%" + req.params.name + "%':: varchar"
+            )
             .then(function (data) {
                 res.json({
                     status: "success",
                     data: data,
                 });
             })
-            .catch((err) => {
-                res.json({
-                    description: "Can't find any controller",
-                    error: err,
-                });
+            .catch(error => {
+                if (Array.isArray(error) && 'getErrors' in error) {
+                    // the error came from method `batch`;
+                    // let's log the very first error:
+                    error = error.getErrors()[0];
+                }
+                console.log("ERROR:", error.message || error);
             });
     });
 
-    // Näidata, millised andurid on konkreetse kontrolleriga ühendatud
-    app.get("/api/controller/:number/sensors", function (req, res) {
+    // c. näidata täielikku teavet määratud linna kohta (2 GET - päringut: linnakoodi ja nime järgi) 
+    app.get("/api/citybyid/:id", function (req, res) {
         db.any(
-        "SELECT sensor.sensorname FROM sensor INNER JOIN controller_sensor ON controller_sensor.id_sensor=sensor.id " +
-        "WHERE controller_sensor.id_controller=" + req.params.number + ":: integer"
-        )
-        .then(function (data) {
-            res.json({
-                status: "success",
-                data: data,
-            });
-        })
-        .catch(function (err) {
-            return next(err);
-        });
-    });
-
-    // Näidata auditooriumi nr 44 andurite andmeid täna
-    app.get("/api/room44today", function (req, res) {
-        db.any(
-        "SELECT datasensor.date_time, datasensor.data, typevalue.dimension, typevalue.valuetype, c1.sensorname, c1.room FROM datasensor " +
-        "INNER JOIN (SELECT controller_sensor.id, controller_sensor.room, sensor.sensorname FROM controller_sensor " +
-		"               INNER JOIN sensor ON sensor.id=controller_sensor.id_sensor) c1 " +
-	    "    ON c1.id=datasensor.id_controllersensor " +
-        "INNER JOIN typevalue ON typevalue.id=datasensor.id_typevalue " +
-        "WHERE c1.room=44::varchar AND datasensor.date_time::date = CURRENT_DATE + 0 " +
-        "ORDER BY datasensor.date_time DESC"
-        )
-        .then(function (data) {
-            res.json({
-                status: "success",
-                data: data,
-            });
-        })
-        .catch(function (err) {
-            return next(err);
-        });
-    });
-
-    // * Näidata, millised controller_sensors on andmebaasis
-    app.get("/api/controller_sensors", function (req, res) {
-        db.any("SELECT DISTINCT controller_sensor FROM controller_sensor")
+            "SELECT id, trim(from name) city_name, countrycode, trim(from district) city_district, population FROM city WHERE city.id=" + req.params.id + ":: integer"
+            )
             .then(function (data) {
                 res.json({
                     status: "success",
                     data: data,
                 });
             })
-            .catch((err) => {
-                res.json({
-                    description: "Can't find any controller_sensor",
-                    error: err,
-                });
+            .catch(error => {
+                if (Array.isArray(error) && 'getErrors' in error) {
+                    // the error came from method `batch`;
+                    // let's log the very first error:
+                    error = error.getErrors()[0];
+                }
+                console.log("ERROR:", error.message || error);
             });
     });
+
+
+    
 };
